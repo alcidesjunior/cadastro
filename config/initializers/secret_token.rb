2e035a6bcff4110d5ca4511b62e02f568ab73a12
alcides <alcidesjunior.infor@gmail.com>
2015-05-15 16:53:18 -0300

    Programa Crud de cadastros

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cadastro::Application.config.secret_key_base = '324d6951dda02d4f6abe3f055737a690ef4b0bb7897fdd5b23908c4d3ef4e2bc806e60811be2d8f0353f32ceb32a29dd13578aab7948c7ece938347e21ad5c60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cadastro::Application.config.session_store :cookie_store, key: '_cadastro_session'
